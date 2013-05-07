d140f9945633bc72bfad41139252371682aafcbe
Jonathan Yoder <jonyoder@gmail.com>
2013-05-07 15:24:02 -0500

    Update RESTful Service for Hibernate Event Interception.

--- guiberest-server/src/main/java/edu/swmed/qbrc/guiberest/guice/DispatchServletModule.java
+++ guiberest-server/src/main/java/edu/swmed/qbrc/guiberest/guice/DispatchServletModule.java
@@ -3,7 +3,7 @@ package edu.swmed.qbrc.guiberest.guice;
 import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 import com.google.inject.Singleton;
 import com.google.inject.servlet.ServletModule;
-
+import edu.swmed.qbrc.auth.cashmac.server.filters.CasHmacRequestFilter;
 import edu.swmed.qbrc.guiberest.guice.datasources.GuiberestDataSourcePersistModule;
 
 public class DispatchServletModule extends ServletModule {
@@ -14,9 +14,13 @@ public class DispatchServletModule extends ServletModule {
 		/* Serve all requests with RESTEasy's HttpServletDispatcher */
 		bind(HttpServletDispatcher.class).in(Singleton.class);
 		serve("/*").with(HttpServletDispatcher.class);
+		
+		/* Bind CasHmacRequestFilter so CasHmac's Hibernate interceptor can access the session for user information. */
+		bind(CasHmacRequestFilter.class).in(Singleton.class);
 
 		/* Filter all requests through our custom PersistModule (sets up Hibernate
 		 * environment automatically). */
         filter("/*").through(GuiberestDataSourcePersistModule.GUIBEREST_DATA_SOURCE_FILTER_KEY);
+        filter("/*").through(CasHmacRequestFilter.class);
     }
 }
